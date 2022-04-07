9b175bb345d201304a0a43311b513476de7886e1
ALISON <superalison>
2022-04-07 15:18:11 -0300

    create connection DB

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const conexao = require('./conexao');
 const router = require('./router');
 
+const port = 3000
 const app = express()
 
 app.use( router )
 
-app.listen(3000,()=>{
-    console.log('sever port 3000')
+app.listen(port,async ()=>{    
+    console.log("running port " + port);
 })
