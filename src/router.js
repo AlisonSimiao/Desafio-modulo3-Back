a33354e26528cd61c6c03f1840bae382769f943d
ALISON <superalison>
2022-04-08 00:08:07 -0300

    create route login/cadastro/usuario

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
+const { signIn, signUp, profile } = require('./controllers/user');
+const validacao = require("./middlewares/validacao")
 const router = express()
 router.use( express.json() )
 
+
+router.get("/usuario", profile);
+router.post("/usuario", validacao.userEmailSenha,signUp);
+
+router.post("/login", validacao.userEmailSenha, signIn);
+        
 module.exports = router;
