78d76974e4b5bd52a7ff88cb2fb06c0310631dd1
ALISON <superalison>
2022-04-09 14:28:05 -0300

    add route /transacao

--- src/router.js
+++ src/router.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const user = require('./controllers/user');
-const category = require('./controllers/category');
+const express   = require('express');
+const user      = require('./controllers/user');
+const category  = require('./controllers/category');
+const deal      = require('./controllers/deal');
 const validacao = require("./middlewares/validacao")
-const router = express()
+const router    = express()
+
+
 router.use( express.json() )
 
 //Route user
 router.get("/usuario", user.profile);
 router.post("/usuario", validacao.userNome,validacao.userEmailSenha,user.signUp);
 router.put("/usuario", validacao.userNome,validacao.userEmailSenha,user.update);
-router.post("/login", validacao.userEmailSenha, user.signIn);
-        
+router.post("/login", validacao.userEmailSenha, user.signIn);    
 
 //Route category
 router.get("/categoria", category.list);
+
+//Route transicion
+router.get("/transacao", deal.list);
+
 module.exports = router;
