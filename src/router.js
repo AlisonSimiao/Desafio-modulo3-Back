27f03002406e3e5944846015c460a8d0f631fc3e
ALISON <superalison>
2022-04-09 14:15:57 -0300

    add route /categoria

--- src/router.js
+++ src/router.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const user = require('./controllers/user');
+const category = require('./controllers/category');
 const validacao = require("./middlewares/validacao")
 const router = express()
 router.use( express.json() )
 
-
+//Route user
 router.get("/usuario", user.profile);
 router.post("/usuario", validacao.userNome,validacao.userEmailSenha,user.signUp);
 router.put("/usuario", validacao.userNome,validacao.userEmailSenha,user.update);
 router.post("/login", validacao.userEmailSenha, user.signIn);
         
+
+//Route category
+router.get("/categoria", category.list);
 module.exports = router;
