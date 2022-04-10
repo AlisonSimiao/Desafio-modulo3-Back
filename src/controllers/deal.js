b56e4ab3aa363b388490835ef47e34d1f7c7d23c
ALISON <superalison>
2022-04-10 18:26:03 -0300

    create controllers byID

--- src/controllers/deal.js
+++ src/controllers/deal.js
@@ -16,6 +16,22 @@ const list = async (req, res) => {
     }
 }
 
+const byId = async (req, res) => {
+    const token = req.headers.authorization.replace("Bearer ", "");
+    const {id: dealID}  = req.params ;
+
+    try {
+        const { id } = jwt.verify(token, SECRET);
+        const { rows } = await conn.query("Select * from transacoes where id = $1 and $2 = usuario_id ",[dealID,id])
+         
+        res.status(200).json(rows);
+    }
+    catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
+
 module.exports = {
-    list
+    list,
+    byId
 }
